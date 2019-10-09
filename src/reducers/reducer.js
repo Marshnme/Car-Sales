5f1ff8a9ca5149288bbff0f50ca4534ceb3a342c
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-10-09 16:32:47 -0400

    data added to addedfeatures (hopefully)

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -9,7 +9,7 @@ const initialState= {
             'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
         features: []
     },
-    additionalFeatures: [
+    store: [
         { id: 1, name: 'V-6 engine', price: 1500 },
         { id: 2, name: 'Racing detail package', price: 1500 },
         { id: 3, name: 'Premium sound system', price: 500 },
@@ -24,7 +24,7 @@ export const reducer = (state = initialState , action) => {
     switch(action.type){
 
 
-        
+
         default:
         return state;
     }
